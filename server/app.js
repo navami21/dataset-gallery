48dafa6749e8f2089369c7cddbbf47df4464cfba
navami21 <navamib2023@gmail.com>
2025-07-28 15:23:44 +0530

    Server updated

--- server/app.js
+++ server/app.js
@@ -1,34 +1,4 @@
-// const express=require('express'); 
-// const morgan=require('morgan');
-// require('dotenv').config();
-// require('./db/connection');
-// const cors =require('cors');
 
-// const PORT = process.env.PORT;
-// const app=new express(); 
-// app.use(express.json());
-// app.use(morgan('dev'));
-// app.use(cors());
-// app.use("/uploads", express.static("uploads"));
-
-
-// const userRoutes=require('./routes/userRoutes');
-// app.use('/api/users',userRoutes)
-// const adminRoutes=require('./routes/adminRoutes');
-// app.use('/api/admin',adminRoutes)
-// const categoryRoutes=require('./routes/categoryRoutes');
-// app.use('/api/category',categoryRoutes)
-// const datasetRoutes=require('./routes/datasetRoutes');
-// app.use('/api/datasets',datasetRoutes)
-// const projectRoutes=require('./routes/projectRoutes');
-// app.use('/api/projects',projectRoutes)
-
-
-
-
-// app.listen(PORT,()=>{
-//     console.log(` ${PORT} is up and running `);
-// })
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
